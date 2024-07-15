0fb64057a54e681db49b9e0b0a776315ecafa56b
Roshan Binoj <roshanbinoj@archlinux.localdomain>
2024-07-16 01:31:15 +0530

    day3

--- best-time-to-buy-and-sell-stock.cpp
+++ best-time-to-buy-and-sell-stock.cpp
@@ -5,9 +5,8 @@ public:
         vector <int> dp(n+1,0);
         int mn = prices[0];
         for(int i=1;i<=n;i++)
-        {
-            dp[i] = max(prices[i]-mn,dp[i-1]);
-            mn = min(mn,prices[i]);
+        {   mn = min(mn,prices[i-1]);
+            dp[i] = max(prices[i-1]-mn,dp[i-1]);
         }
         return dp[n];
     }
