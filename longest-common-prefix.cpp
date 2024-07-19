e50997e1649c6e13fbf61d0dc2f1894d6d929f1a
Roshan Binoj <roshanbinoj@archlinux.localdomain>
2024-07-19 22:31:24 +0530

    day6

---
+++ longest-common-prefix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int n = strs.size();
+        sort(strs.begin(), strs.end());
+        string first = strs[0], last = strs[n - 1];
+        string ans = "";
+        for (int i = 0; i < min(first.size(), last.size()); i++) {
+            if (first[i] != last[i])
+                return ans;
+            else
+                ans += first[i];
+        }
+        return ans;
+    }
+};
